fb9fb6f36ec987580c9e105905a764eadcbdb08e
slavik <slavik.zaliotnyi@gmail.com>
2022-09-27 15:34:52 +0300

    added modal add

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('DMContentLoaded',() =>{
+window.addEventListener('DOMContentLoaded',() =>{
 
 
 
@@ -19,6 +19,37 @@ window.addEventListener('DMContentLoaded',() =>{
 
 
 
+/////Modal logik
 
-  
+  const btnAdd = document.querySelector('[data-add]'),
+    modal = document.querySelector('.modal'),
+    modalClose = document.querySelector('[data-close]'),
+    body = document.querySelector('body');
+
+  function showModal(modalWindow) {
+    modalWindow.classList.add('show');
+    modalWindow.classList.remove('hide');
+    body.style.overflow = 'hidden';
+  }
+
+  function closeModal(modalWindow) {
+    modalWindow.classList.remove('show');
+    modalWindow.classList.add('hide');
+    body.style.overflow = '';
+  }
+
+  btnAdd.addEventListener('click',() =>{
+    showModal(modal);
+  });
+
+  modal.addEventListener('click', function (e) {
+    const target = e.target;
+    if (target === modal) {
+      closeModal(modal);
+    }
+  });
+
+  modalClose.addEventListener('click',()=>{
+    closeModal(modal);
+  });
 });
