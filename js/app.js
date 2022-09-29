c8d48ae4184630474bfe21711ddd0210566361c2
slavik <slavik.zaliotnyi@gmail.com>
2022-09-29 19:15:00 +0300

    added func of go task in pgogress list

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
   /*Логіка */
 
-
-
   const allListParent = document.querySelector('.content__todo-list'),
     addListBtn = document.querySelector('[data-add-new]'),
     formAdd = document.querySelector('[data-form-add]'),
     closeModalDeleteBtn = document.querySelector('[data-close-delete-modal]'),
-    deleteOn = document.querySelector('.delete-on');
+    deleteOn = document.querySelector('.delete-on'),
+    inProgresList = document.querySelector('.in-progres-list'),
+    doneModal = document.querySelector('.done-modal'),
+    doneBtn = document.querySelector('.done-btn');
   // let allLi = document.querySelectorAll('.all-js');
   let nameTodo = document.querySelector('[data-name]');
   let textTodo = document.querySelector('[data-text]');
 
 
   let todoList = [];
+  let progresTodoList = [];
   let count;
   count = todoList.length;
 
@@ -29,7 +31,30 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  if (localStorage.getItem('progres')) {
+    progresTodoList = JSON.parse(localStorage.getItem('progres'));
+    updateList();
+    updateProgresList();
+  }
 
+
+
+  addListBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (nameTodo.value && nameTodo.value.match(/^\s+$/) === null) {
+      let newTtodo = {
+        todoName: nameTodo.value,
+        todoDescr: textTodo.value
+      };
+      todoList.push(newTtodo);
+      localStorage.setItem('todo', JSON.stringify(todoList));
+      updateList();
+      formAdd.reset();
+      closeModal(modal);
+    } else {
+      alert('Введіть назву завдання')
+    }
+  });
 
 
   function updateList() {
@@ -44,7 +69,6 @@ window.addEventListener('DOMContentLoaded', () => {
     /*Кількість невиконаних справ */
     count = todoList.length;
     document.querySelector('.content__todo-block-all').innerHTML = count;
-    console.log(count);
     todoList.forEach((item, i) => {
       todoLi += `
    <li class="content__todo-list__li all-js">
@@ -68,6 +92,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
+    /*Видалення справи */
     document.querySelectorAll('[data-deleteAll]').forEach((item, i) => {
       function deleteTaskLocal(e) {
         e.preventDefault();
@@ -89,26 +114,114 @@ window.addEventListener('DOMContentLoaded', () => {
         });
       });
     });
+    /*Початок виконання справи */
+    document.querySelectorAll('[data-go]').forEach((item, i) => {
+      function addTaskToInProgress(e) {
+        e.preventDefault();
+        let copyProgressObj = JSON.parse(JSON.stringify(todoList[i]));
+        progresTodoList.push(copyProgressObj);
+        updateProgresList();
+        todoList.splice(i, 1);
+        updateList();
+        closeModal(runModal);
+        runBtn.removeEventListener('click', addTaskToInProgress);
+        localStorage.setItem('todo', JSON.stringify(todoList));
+        localStorage.setItem('progres', JSON.stringify(progresTodoList));
+      }
+      item.addEventListener('click', (e) => {
+        e.preventDefault();
+        showModal(runModal);
+        console.log(todoList);
+        console.log(progresTodoList);
+        runBtn.addEventListener('click', addTaskToInProgress);
+        closeRunModal.addEventListener('click', (e) => {
+          e.preventDefault();
+          closeModal(runModal);
+          runBtn.removeEventListener('click', addTaskToInProgress);
+        });
+      });
+    });
   }
 
-  addListBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (nameTodo.value && nameTodo.value.match(/^\s+$/) === null) {
-      let newTtodo = {
-        todoName: nameTodo.value,
-        todoDescr: textTodo.value
-      };
-      todoList.push(newTtodo);
-      localStorage.setItem('todo', JSON.stringify(todoList));
-      updateList();
-      formAdd.reset();
-      closeModal(modal);
-    } else {
-      alert('Введіть назву завдання')
+
+
+  function updateProgresList() {
+    inProgresList.innerHTML = '';
+    if (progresTodoList.length == 0) {
+      const message = document.createElement('p');
+      message.classList.add('message');
+      message.innerHTML = 'Почніть додавати справи, щоб їх виконувати';
+      inProgresList.append(message);
     }
-  });
+    /*Кількість невиконаних справ */
+    count = progresTodoList.length;
+    document.querySelector('.content__todo-block-inprogress').innerHTML = count;
+    let progresLi = '';
+    progresTodoList.forEach((item, i) => {
+      progresLi += `
+      <li class="content__todo-list__li in-progress">
+            <div class="content__todo-top__wrapper">
+              <p class="content__todo-name">${item.todoName}</p>
+              <div class="content__todo-btns">
+                <button data-done-task class="content__todo-go btns">Завершити</button>
+                <button data-inProgress class="content__todo-delete btns">Видалити</button>
+                <div class="content__todo-status_ball progress"></div>
+              </div>
+            </div>
+            <div class="content__todo-botton__wrapper">
+              <p class="content__todo-text">
+                 ${item.todoDescr}
+              </p>
+            </div>
+          </li>
+      `;
+      inProgresList.innerHTML = progresLi;
 
+      /*видалення справи*/
+      document.querySelectorAll('[data-inProgress]').forEach((item, i) => {
+        function deleteTaskLocal(e) {
+          e.preventDefault();
+          closeModal(deleteModal);
+          progresTodoList.splice(i, 1);
+          updateProgresList();
+          localStorage.setItem('progres', JSON.stringify(progresTodoList));
+          deleteOn.removeEventListener('click', deleteTaskLocal);
+        }
+        item.addEventListener('click', function (e) {
+          e.preventDefault();
+          showModal(deleteModal);
+          deleteOn.addEventListener('click', deleteTaskLocal);
+          closeModalDeleteBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            closeModal(doneModal);
+            item.removeEventListener('click', deleteTaskLocal);/*перевірка на відхилення додання завдання*/
+          });
+        });
+      });
 
+      /*Завешеня виконання справи */
+      document.querySelectorAll('[data-done-task]').forEach((item, i) => {
+        function doneTask(e) {
+          e.preventDefault();
+          closeModal(doneModal);
+          progresTodoList.splice(i, 1);
+          updateProgresList();
+          localStorage.setItem('progres', JSON.stringify(progresTodoList));
+          doneBtn.removeEventListener('click', doneTask);
+        }
+        item.addEventListener('click', function (e) {
+          e.preventDefault();
+          showModal(doneModal);
+          doneBtn.addEventListener('click', doneTask);
+          closeModalDeleteBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            closeModal(doneModal);
+            item.removeEventListener('click', doneTask);/*перевірка на відхилення */
+          });
+        });
+      });
+    });
+  }
 
 
   /*змінні для видалення*/
@@ -120,8 +233,7 @@ window.addEventListener('DOMContentLoaded', () => {
     deleteModal = document.querySelector('.delete-modal'),
     runModal = document.querySelector('.run-modal'),
     runBtn = document.querySelector('.run-on'),
-    closeRunModal = document.querySelector('[data-close-run-modal]'),
-    runGoBtns = document.querySelectorAll('[data-go]');
+    closeRunModal = document.querySelector('[data-close-run-modal]');
 
 
 
@@ -202,25 +314,6 @@ window.addEventListener('DOMContentLoaded', () => {
   /*функ початку виконання завдання*/
 
 
-
-  runGoBtns.forEach((item, i) => {
-    function addTaskToInProgress(e) {
-      e.preventDefault();
-      deleteLi(allLi, i);
-      closeModal(runModal);
-    }
-    item.addEventListener('click', (e) => {
-      e.preventDefault();
-      showModal(runModal);
-
-      runBtn.addEventListener('click', addTaskToInProgress);
-      closeRunModal.addEventListener('click', (e) => {
-        e.preventDefault();
-        closeModal(runModal);
-        runBtn.removeEventListener('click', addTaskToInProgress);
-      });
-    });
-  });
 
 
 
